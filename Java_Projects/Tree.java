539c49ca986a73640901c73b7215910b208f66ef
stevenmajor88 <stevenmajor88@yahoo.com>
2023-05-07 18:27:53 -0500

    Initial commit

---
+++ Java_Projects/Tree.java
@@ -0,0 +1,23 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args) {
+        // introduce ourselves and print a tree to the screen
+        System.out.println("Hey there, I'm Steven Nguyen, and I'm learning to code in Java!");
+        System.out.println("I'm going to plant a tree today!");
+
+        System.out.println("  *  ");
+        System.out.println("  ***  ");
+        System.out.println("  ***  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+        System.out.println();
+
+        System.out.println("  *  ");
+        System.out.println("  ******  ");
+        System.out.println("  ****  ");
+        System.out.println("  ***  ");
+        System.out.println("  **  ");
+
+    }
+}
